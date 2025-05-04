b82daff19ffd54936dd797fcefac97f6930ed031
Drew Dalmedo <andrewdalmedo@gmail.com>
2025-05-04 12:44:36 -0400

    remove cd test

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from dash import Dash, html
 
 app = Dash(__name__)
-app.layout = html.Div(children=['Hello, world!', 'This is a test for CD'])
+app.layout = html.Div(children=['Hello, world!'])
 
 if __name__ == '__main__':
     app.run(debug=False)
