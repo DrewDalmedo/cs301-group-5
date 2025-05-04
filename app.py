8a6e70b1915d8865eb5fdf9af5f4c16c7159392a
Drew Dalmedo <andrewdalmedo@gmail.com>
2025-05-04 12:38:03 -0400

    add extra element to div for cd test

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from dash import Dash, html
 
 app = Dash(__name__)
-app.layout = html.Div('Hello, world!')
+app.layout = html.Div(children=['Hello, world!', 'This is a test for CD'])
 
 if __name__ == '__main__':
     app.run(debug=False)
